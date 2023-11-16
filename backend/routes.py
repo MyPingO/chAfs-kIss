5b3fd2c0a3e91f7ccd58506b52dfa1131bedd502
MyPing0 <myping0yt@gmail.com>
2023-11-15 21:12:21 -0500

    Replaced basic Flask setup with basic FastAPI setup

--- backend/routes.py
+++ backend/routes.py
@@ -1,19 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from backend.app import app
+from fastapi import APIRouter
 
-# Flask routes
-@app.route('/')
-def index():
-    return 'Hello, World!'
+router = APIRouter()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return 'Login'
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logged out successfully.')
-    return redirect(url_for('index'))
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
