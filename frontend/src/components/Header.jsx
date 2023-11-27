98b92d0de1a4d1894ba05eac636c0c98278cceaa
joshkat <joshua02k@gmail.com>
2023-11-27 09:27:57 -0500

    Imported fonts and finished header TODO: Change header depending on screen size

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <div className="bg-red-300 flex justify-evenly items-center h-24">
-        <Link to={"/about"} className="">
+      <div className="flex justify-evenly items-center h-24 font-inter">
+        <Link to={"/about"} className="font-Inter">
           About
         </Link>
         <Link to={"/pricing"} className="">
           Pricing
         </Link>
-        <Link to={"/"} className="">
+        <Link to={"/"} className="font-kuashan text-sky-400 text-3xl">
           {"chAf's kIss"}
         </Link>
         <Link to={"/contact"} className="">
@@ -20,8 +20,12 @@ function Header() {
           Terms
         </Link>
       </div>
-      <p>Your AI Cooking Assistant</p>
-      <p>Perfect for figuring out what to make for ANY occasion!</p>
+      <div className="text-center">
+        <p className="font-inter">Your AI Cooking Assistant</p>
+        <p className="font-bold">
+          Perfect for figuring out what to make for ANY occasion!
+        </p>
+      </div>
     </header>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        kuashan: ["Kaushan Script", "cursive"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
