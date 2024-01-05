a014103c6377784832fbf4adf6936e7e9d1f105c
Josh <joshua02k@gmail.com>
2024-01-04 20:42:51 -0500

    Connected backend to generate responses Used temporary server link

--- frontend/src/components/Home/ResponseArea.jsx
+++ frontend/src/components/Home/ResponseArea.jsx
@@ -1,19 +1,40 @@
 import ResponseList from "./ResponseList";
 import PropTypes from "prop-types";
 
-function ResponseArea({ setInputView, responses, setResponses }) {
-  function handleSubmit() {
+function ResponseArea({ setInputView, responses, setResponses, badInputs }) {
+  async function handleSubmit() {
     const foodInput = document.getElementById("food-input");
+    const restrictions = [...badInputs];
+
+    setResponses(["loading"]);
     if (foodInput.value === "") return;
     foodInput.disabled = true;
-    // do something in the backend with the filters to get back the response ideas
-    // for now using dummy data
-    setResponses([
-      "Sesame Chicken",
-      "Chicken Lo Mein",
-      "Shrimp Fried Rice",
-      "Hot and Sour Soup",
-    ]);
+
+    const server_url = "https://07wljc9s-8000.use.devtunnels.ms";
+    // Hardcoded meal query data
+    const mealQueryData = {
+      query: foodInput.value,
+      restrictions: restrictions,
+    };
+
+    const data = fetch(`${server_url}/generate_meals`, {
+      method: "POST",
+      body: mealQueryData,
+    });
+    try {
+      const res = await data;
+      console.log(res);
+      return;
+      // setResponses(res.response);
+    } catch (err) {
+      console.log(err, data);
+    }
+    // setResponses([
+    //   "Sesame Chicken",
+    //   "Chicken Lo Mein",
+    //   "Shrimp Fried Rice",
+    //   "Hot and Sour Soup",
+    // ]);
   }
   return (
     <div className="join join-vertical w-full h-full">
@@ -44,6 +65,13 @@ function ResponseArea({ setInputView, responses, setResponses }) {
           >
             Submit
           </button>
+        ) : responses[0] === "loading" ? (
+          <>
+            <div className="flex items-center">
+              <p className="font-bold text-lg ">Thinking</p>
+              <span className="loading loading-dots loading-lg mt-2"></span>
+            </div>
+          </>
         ) : (
           <ResponseList responses={responses} setInputView={setInputView} />
         )}
@@ -56,6 +84,7 @@ ResponseArea.propTypes = {
   setInputView: PropTypes.func.isRequired,
   responses: PropTypes.array.isRequired,
   setResponses: PropTypes.func.isRequired,
+  badInputs: PropTypes.object.isRequired,
 };
 
 export default ResponseArea;
--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -19,6 +19,7 @@ function Home() {
         <div className="w-full h-full mr-2">
           <ResponseArea
             responses={responses}
+            badInputs={badInputs}
             setResponses={setResponses}
             setInputView={setInputView}
           />
