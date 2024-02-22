1535a841e35ad6cc459555bfcb87d6755629ff01
Josh <joshua02k@gmail.com>
2024-02-21 21:31:46 -0500

    Added back call to server to generate_meals

--- frontend/src/components/Home/ResponseArea.jsx
+++ frontend/src/components/Home/ResponseArea.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ResponseList from "./ResponseList";
 import PropTypes from "prop-types";
 
@@ -9,6 +10,7 @@ function ResponseArea({
   setResponses,
   badInputs,
 }) {
+  const [inputLength, setInputLength] = useState(0);
   async function handleSubmit() {
     const foodInput = document.getElementById("food-input");
     const restrictions = [...badInputs];
@@ -17,16 +19,14 @@ function ResponseArea({
     foodInput.disabled = true;
     setResponses(["loading"]);
 
-    setResponses([
-      "Sesame Chicken",
-      "Chicken Lo Mein",
-      "Shrimp Fried Rice",
-      "Hot and Sour Soup",
-    ]);
+    // setResponses([
+    //   "Sesame Chicken",
+    //   "Chicken Lo Mein",
+    //   "Shrimp Fried Rice",
+    //   "Hot and Sour Soup",
+    // ]);
 
-    /*
-    const server_url = "https://07wljc9s-8000.use.devtunnels.ms";
-    // Hardcoded meal query data
+    const server_url = import.meta.env.VITE_SERVER_URL;
 
     const data = fetch(`${server_url}/generate_meals`, {
       method: "POST",
@@ -38,29 +38,49 @@ function ResponseArea({
         restrictions: restrictions,
       }),
     });
+
     try {
       const res = await data;
-      console.log(res);
-      return;
-      // setResponses(res.response);
+      const resData = await res.json();
+      setResponses(resData.response);
     } catch (err) {
-      console.log(err, data);
+      console.log(err);
     }
-    */
   }
   return (
     <div className="join join-vertical w-full h-full">
       <div className="join-item">
         <div className="join w-full">
-          <input
-            type="text"
-            className="input w-full input-bordered join-item"
-            style={{ borderTopRightRadius: "0px" }}
-            placeholder="What would you like to eat?"
-            id="food-input"
-          />
+          <label className="flex flex-col w-full">
+            <textarea
+              className="input w-full input-bordered join-item h-[4.5rem] resize-none"
+              rows={3}
+              style={{
+                borderTopRightRadius: "0px",
+                borderBottomLeftRadius: ".5rem",
+              }}
+              placeholder="What would you like to eat?"
+              id="food-input"
+              maxLength="351"
+              onInput={e => {
+                const newInputLength = e.target.value.length;
+                const input = e.target;
+                setInputLength(newInputLength);
+
+                if (inputLength === 349) {
+                  input.classList.add("textarea-error");
+                } else {
+                  input.classList.remove("textarea-error");
+                }
+              }}
+            />
+            <span className="label-text-alt mb-2">
+              Char Count: {inputLength} / 350
+            </span>
+          </label>
           <button
-            className="btn btn-error rounded-none rounded-tr-lg text-white"
+            className="btn btn-error rounded-none rounded-tr-lg rounded-br-lg text-white"
+            style={{ height: "4.5rem" }}
             onClick={() => {
               const foodInput = document.getElementById("food-input");
               foodInput.disabled = false;
